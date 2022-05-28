239784fcf42af747a779cf96b6c4376ea095e958
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-28 16:30:03 -0300

    Create select.js

---
+++ select.js
@@ -0,0 +1,15 @@
+var database = require('./database');
+
+// Select All Data
+database.select().table('product').then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err)
+})
+
+// Select index
+database.select(['id', 'price']).table('product').then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err)
+})
