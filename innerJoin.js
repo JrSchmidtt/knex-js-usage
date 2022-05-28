ccc35e1d9dd2ad4137139991a3b4fcaef7e15d12
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-28 18:14:51 -0300

    Create innerJoin.js

---
+++ innerJoin.js
@@ -0,0 +1,14 @@
+var database = require('./database');
+
+query = [
+    'product.id as product_id',
+    'product.name as product_name',
+    'store.id as store_id',
+    'store.name as store_name'
+]
+
+database.select(query).table('product').innerJoin('store','store.product_id','product.id').then(data =>{
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
